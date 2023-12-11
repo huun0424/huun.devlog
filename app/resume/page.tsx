f434937bf42cdf94bccfbd49c5797e9fd2b40bdd
huun0424 <huunl91@naver.com>
2023-12-11 23:41:00 +0900

    - feat: resume update.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,16 +4,16 @@ import ResumeLayout from '@/layouts/ResumeLayout';
 import { coreContent } from 'pliny/utils/contentlayer';
 import { genPageMetadata } from 'app/seo';
 
-export const metadata = genPageMetadata({ title: 'About' });
+export const metadata = genPageMetadata({ title: 'Resume' });
 
 export default function Page() {
-  const author = allResumes.find((p) => p.slug === 'default') as Resume;
-  const mainContent = coreContent(author);
+  const resume = allResumes.find((p) => p.slug === 'default') as Resume;
+  const mainContent = coreContent(resume);
 
   return (
     <>
       <ResumeLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
+        <MDXLayoutRenderer code={resume.body.code} />
       </ResumeLayout>
     </>
   );
