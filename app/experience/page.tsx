b3aa8e6813b36de60cd2271ddfc84261818ce2da
huun0424 <huunl91@naver.com>
2023-12-14 00:18:03 +0900

    - feat: experience main page mdx post 형태로 데이터 변경.

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,13 +1,15 @@
 import { genPageMetadata } from 'app/seo';
 import { Metadata } from 'next';
 import ExperienceLayout from '@/layouts/ExperienceLayout';
+import { allExperiences } from 'contentlayer/generated';
 
 export async function generateMetadata(): Promise<Metadata> {
   return genPageMetadata({
     title: 'Experience',
+    description: `All Work Experience`,
   });
 }
 
 export default function Experience() {
-  return <ExperienceLayout />;
+  return <ExperienceLayout posts={allExperiences.sort((postA, postB) => postB.seq - postA.seq)} />;
 }
