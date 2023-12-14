cfcf8400ddd1b7a69c9770072f0bbaed05859321
huun0424 <huunl91@naver.com>
2023-12-14 19:23:15 +0900

    - hotfix: resume 임시 페이지로 href 수정.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,7 +4,11 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/experience', title: 'Experience' },
-  { href: '/resume', title: 'Resume' },
+  // { href: '/resume', title: 'Resume' },
+  {
+    href: 'https://slender-larch-5c2.notion.site/b235e1aaa95d4660917fca7cf1313c4d',
+    title: 'Resume',
+  },
 ];
 
 export default headerNavLinks;
